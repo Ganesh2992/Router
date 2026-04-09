26798a40abf3aaec936e6d2354f5b773f4509036
Ganesh Mangalam Sinha <ganeshmangalamsinha@gmail.com>
2026-04-09 09:49:14 +0530

    Added the another config

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData,githubInfoLoader } from 'react-router-dom';
 
 function Github(){
     const data = useLoaderData()
